d7139d7cd940aee2c1b2f3c52e9f318072c22746
Dmitry Maksakov <d.maksakov@sbereducation.ru>
2023-05-22 13:53:15 +0300

    debug

--- JOB1.groovy
+++ JOB1.groovy
@@ -52,7 +52,8 @@ pipeline {
             steps {
                 script {
                     if (currentBuild.getBuildCauses('hudson.model.Cause$UserIdCause')) {
-                        currentBuild.result = hudson.model.Result.SUCCESS.toString()
+                        currentBuild.result = 'ABORTED'
+
                     }
                 }
             }
