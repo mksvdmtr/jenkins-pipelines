fd982c6fbd90a81c3b69d230c515a8ddb4d58385
Dmitry Maksakov <d.maksakov@sbereducation.ru>
2023-05-24 14:17:34 +0300

    debug

--- JOB2.groovy
+++ JOB2.groovy
@@ -2,7 +2,7 @@ properties([
     parameters([
         string(
             name: 'DOCKER_TAG',
-            defaultValue: 'master_c9ecef82703__STABLE1111',
+            defaultValue: 'master_c9ecef82703__STABLE2222',
             description: 'Необходимо ввести DOCKER TAG, собранного релиза, который ранее устанавливался на DEV стенды'
         )
     ])
