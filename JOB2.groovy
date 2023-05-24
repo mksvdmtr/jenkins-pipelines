d8cfeae87661f07d23a1aea00dddadd025ade39f
Dmitry Maksakov <d.maksakov@sbereducation.ru>
2023-05-24 14:01:12 +0300

    add job2

---
+++ JOB2.groovy
@@ -0,0 +1,21 @@
+properties([
+    parameters([
+        string(
+            name: 'DOCKER_TAG',
+            defaultValue: 'master_c9ecef82703__STABLE3',
+            description: 'Необходимо ввести DOCKER TAG, собранного релиза, который ранее устанавливался на DEV стенды'
+        )
+    ])
+])
+
+pipeline {
+    agent any
+
+    stages {
+        stage('Hello') {
+            steps {
+                echo "Hello World From JOB2 - DOCKER_TAG: ${params.DOCKER_TAG}"
+            }
+        }
+    }
+}
