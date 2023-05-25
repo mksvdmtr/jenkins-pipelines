9967670dfd0294273c6be3153cf123e2077dc793
Dmitry Maksakov <d.maksakov@sbereducation.ru>
2023-05-25 09:47:56 +0300

    debug

--- JOB2.groovy
+++ JOB2.groovy
@@ -11,10 +11,17 @@ properties([
 pipeline {
     agent any
 
-    stages {
-        stage('Hello') {
-            steps {
-                echo "Hello World From JOB2 - DOCKER_TAG: ${params.DOCKER_TAG}"
+        stages {
+            stage('Hello') {
+                steps {
+                    script {
+                        if triggeredBy(cause: 'UpstreamCause', detail: 'JOB1') {
+                            echo "Hello World From JOB2 - DOCKER_TAG: ${params.DOCKER_TAG}"
+                        } else {
+                            echo "Triggered by something else"
+
+                        }
+                }
             }
         }
     }
