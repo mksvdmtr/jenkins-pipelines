4749fd819bcc7e0e8b8db82e062b63dee9cc3e57
Dmitry Maksakov <d.maksakov@sbereducation.ru>
2023-05-30 09:34:43 +0300

    debug

--- JOB2.groovy
+++ JOB2.groovy
@@ -17,7 +17,8 @@ pipeline {
                     script {
                         def causes = currentBuild.getBuildCauses()
                         echo "${causes}"
-                        if (currentBuild.getBuildCauses()[0].toString().contains('BuildUpstreamCause')) { 
+                        if (currentBuild.getBuildCauses('org.jenkinsci.plugins.workflow.support.steps.build.BuildUpstreamCause'))
+                        // if (currentBuild.getBuildCauses()[0].toString().contains('BuildUpstreamCause')) { 
                             echo "Hello World From JOB2 - DOCKER_TAG: ${params.DOCKER_TAG}"
                         } else {
                             echo "Triggered by something else"
